9564daf42b05d33037190deeb6b222096fd7bdf9
Waseem uUllah Khan <Waseem Ullah khan>
2026-05-05 11:40:23 +0500

    product details page has been added

--- frontend/my-app/src/components/ProductCard.jsx
+++ frontend/my-app/src/components/ProductCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
-  if (!product) return <h1>No Products</h1>;
-
+  const navigate=useNavigate()
+  const ProductDetails=()=>{
+      navigate(`/user/product${product._id}`)
+  }
   return (
     <Link to={`/product/${product._id}`}>
       <div className="bg-white shadow rounded p-4 cursor-pointer hover:shadow-lg text-center transition-shadow">
@@ -12,10 +14,6 @@ const ProductCard = ({ product }) => {
           alt={product.name}
           className="h-40 object-contain w-full"
         />
-        <div className="text-yellow-400 mt-2">
-          {"★".repeat(Math.round(product.rating || 0))}{" "}
-          {product.rating ? `(${product.rating})` : ""}
-        </div>
         <h3 className="text-sm font-medium mt-1 line-clamp-2">
           {product.name}
         </h3>
@@ -23,6 +21,10 @@ const ProductCard = ({ product }) => {
         {product.discount && (
           <p className="text-red-500 text-sm">Discount: {product.discount}%</p>
         )}
+         <div className="text-yellow-400 mt-2">
+          {"★".repeat(Math.round(product.rating || 0))}{" "}
+          {product.rating ? `(${product.rating})` : ""}
+        </div>
       </div>
     </Link>
   );
